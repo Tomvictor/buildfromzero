a105d707045a9cd31b30b573dfd971411ed9e665
Tom Victor <vjtomvictor@icloud.com>
2021-11-13 17:21:53 +0530

    Content Updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,11 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Build From Zero`,
+    siteTitleAlt: `Build From Zero - Learn to build by Tom Victor`,
+    siteHeadline: `Build From Zero - Learn to build by Tom Victor`,
+    siteUrl: `https://buildfromzero.com`,
+    siteDescription: `Learn to build by Tom Victor`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
     author: `@lekoarts_de`,
@@ -26,8 +26,8 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        short_name: `Build FRom Zero`,
+        description: `Learn to build by Tom Victor`,
         start_url: `/`,
         background_color: `#141821`,
         // This will impact how browsers show your PWA/website
